a5f76cd52ee2ea51fff3b92e43a9723f6988313b
yyyannnn <349168872@gapps.yrdsb.ca>
2024-09-24 13:02:56 -0400

    Added start game

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,5 +17,31 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    public void act()
+    {
+        // Start the game if user presses the space bar
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Label titleLabel = new Label("Hungry Animal", 60);
+        addObject(titleLabel,300,104);
+        
+        Label label = new Label("Use \u2190 \u2192 to Move", 40);
+        addObject(label,300,281);
+        
+        Label label2 = new Label("Press <space> to Start", 40);
+        addObject(label2,300,332);
     }
 }
