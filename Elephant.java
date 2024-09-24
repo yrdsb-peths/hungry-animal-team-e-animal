858b3fe28886c54ebb28565fd71db229ba05363a
yyyannnn <349168872@gapps.yrdsb.ca>
2024-09-24 13:15:08 -0400

    Added scoreLabel

--- Elephant.java
+++ Elephant.java
@@ -18,5 +18,6 @@ public class Elephant extends Actor
         {
             move(2);
         }
+        
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    public int score = 0;
+    Label scoreLabel;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -20,6 +23,10 @@ public class MyWorld extends World
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
         
+        // Create label
+        scoreLabel = new Label(0,80);
+        addObject(scoreLabel,50,50);
+        
         createApple();
     }
     
@@ -29,7 +36,15 @@ public class MyWorld extends World
     public void createApple() {
         Apple apple = new Apple();
         addObject(apple, Greenfoot.getRandomNumber(600), 0);      
-        
-        
     }
+        
+    /**
+     * Increase Score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
 }
