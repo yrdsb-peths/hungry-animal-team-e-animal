0f003cb41e64cbefe9f10b09c9fd8d3522884c9f
yyyannnn <349168872@gapps.yrdsb.ca>
2024-09-24 12:59:42 -0400

    Added title

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +16,21 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Label titleLabel = new Label("Hungry Animal", 60);
+        addObject(titleLabel,300,104);
+        
+        Label label = new Label("Use \u2190 \u2192 to Move", 40);
+        addObject(label,300,281);
+        
+        Label label2 = new Label("Press <space> to Start", 40);
+        addObject(label2,300,332);
     }
 }
