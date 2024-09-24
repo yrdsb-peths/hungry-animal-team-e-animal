c9e586cbce6bb556f09891b4e2b7f93e6b735fa4
Jacob Tian <39249179+ilikecandy@users.noreply.github.com>
2024-09-24 12:54:50 -0400

    Add apple

---
+++ Apple.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Apple here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX(), getY() + 2);
+        
+        // Remove apple if reached end
+        MyWorld world = (MyWorld) getWorld();
+        if (getY() >= world.getHeight()) {
+            world.removeObject(this);
+        }
+    }
+}
