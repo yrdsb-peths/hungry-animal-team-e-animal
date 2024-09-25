e4960359559a518ea1d560592021eacc3d6d2008
t1ffanyh <Tiffanyho0903@gmail.com>
2024-09-24 22:07:31 -0400

    eat

--- Elephant.java
+++ Elephant.java
@@ -18,6 +18,15 @@ public class Elephant extends Actor
         {
             move(2);
         }
-        
+        eat();
+    }
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -47,4 +47,6 @@ public class MyWorld extends World
         scoreLabel.setValue(score);
     }
     
+    
+    
 }
