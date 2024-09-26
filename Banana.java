d0b2ada6d6f8a8344325b7713bd7528ed4dcc0f4
yyyannnn <349168872@gapps.yrdsb.ca>
2024-09-26 12:54:54 -0400

    Added movable interface

--- Banana.java
+++ Banana.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Banana extends Food
+public class Banana extends Food implements Movable
 {
     public Banana() {
         super(5);
